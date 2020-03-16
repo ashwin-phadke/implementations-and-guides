06a9d93a10c7f45996d0545effe1c80fb5b7b65f
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-03-16 21:30:18 +0530

    - added sqrt method from math library.

--- programming_basics_in_python/perfect_Square.py
+++ programming_basics_in_python/perfect_Square.py
@@ -1,31 +1,61 @@
-# Initialize variable to iterate over to find perfect square
-ans = 0
+import math 
 
-# Initialize a flag to flag if negative numbers are provided by user
-neg_flag = False
+def isPerfectSquare(x): 
+  
+    # Find floating point value of  
+    # square root of x. 
+    sr = math.sqrt(x) 
+   
+    # If square root is an integer 
+    return ((sr - math.floor(sr)) == 0) , sr
+  
+# Driver code 
+ 
+def perfectSquareSimple():
 
-x = int(input("Enter an integer: "))
+   # Initialize variable to iterate over to find perfect square
+   ans = 0
 
-# Checking if input is negative as negative numbers cannot be perfect squares by definition. 
-if x < 0:
-   neg_flag = True
+   # Initialize a flag to flag if negative numbers are provided by user
+   neg_flag = False
 
-#Iterate squaring all possible values to find square root
-while ans**2 < x:
-   ans = ans + 1
+   x = int(input("Enter an integer: "))
 
-# If square of the result of above iteration is equal to the number from user then the number
-# provided by the user is a perfect square.
-if ans**2 == x:
-   print("Square root of", x, "is", ans)
+   # Checking if input is negative as negative numbers cannot be perfect squares by definition. 
+   if x < 0:
+      neg_flag = True
 
-# Some numbers are not hence the condition for that
-else:
-   print(x, "is not a perfect square")
+   #Iterate squaring all possible values to find square root
+   while ans**2 < x:
+      ans = ans + 1
 
-# if it was not then was it a negative number from user(check)
-   if neg_flag:
-       print("Just checking... did you mean", -x, "?")
+   # If square of the result of above iteration is equal to the number from user then the number
+   # provided by the user is a perfect square.
+   if ans**2 == x:
+      print("Square root of", x, "is", ans)
+
+   # Some numbers are not hence the condition for that
+   else:
+      print(x, "is not a perfect square")
+
+   # if it was not then was it a negative number from user(check)
+      if neg_flag:
+          print("Just checking... did you mean", -x, "?")
 
 
-#MITOCW 6.0001
+   # #MITOCW 6.0001
+
+
+if __name__ == "__main__":
+
+   
+   x = int(input("Enter a value : "))
+   if (isPerfectSquare(x)): 
+      print("Yes", x, "Is a perfect square ") 
+   else: 
+      print("No") 
+
+
+
+
+  
