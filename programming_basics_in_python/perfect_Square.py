ea88f220567880d60f7b6da61ee3016445e8cca9
Ashwin Phadke <ashwinphadke12@rediffmail.com>
2020-03-25 20:48:07 +0530

    - added method selection using argparse.

--- programming_basics_in_python/perfect_Square.py
+++ programming_basics_in_python/perfect_Square.py
@@ -1,4 +1,5 @@
 import math 
+import argparse
 
 def isPerfectSquare(x): 
   
@@ -41,6 +42,8 @@ def perfectSquareSimple():
    # if it was not then was it a negative number from user(check)
       if neg_flag:
           print("Just checking... did you mean", -x, "?")
+   
+   return x
 
 
    # #MITOCW 6.0001
@@ -48,14 +51,31 @@ def perfectSquareSimple():
 
 if __name__ == "__main__":
 
+   #Implement argparse to accept command line arguements to choose methods.
+   parser = argparse.ArgumentParser(description='This program shows the two methods to check if a given number is perfect square')
+
+   parser.add_argument('--method', type=str, help='Define method of finding out perfect square')
+   # , default='simple'
+   args = parser.parse_args()
+
+   #Use simple method
+   if args.method == 'simple' : 
+      y = perfectSquareSimple()
+
+   #Use math.sqrt() way   
+   elif args.method == 'math' :
+      x = int(input("Enter a value : "))
+      if (isPerfectSquare(x)): 
+         print("Yes", x, "Is a perfect square ") 
+      else: 
+         print("No") 
    
-   x = int(input("Enter a value : "))
-   if (isPerfectSquare(x)): 
-      print("Yes", x, "Is a perfect square ") 
-   else: 
-      print("No") 
+   #User did not define a method , display help string for information
+   else:
+      print('Enter a valid method to calculate perfect square. \n 1. Simple mathematical calculation (--method simple) \n 2. Using math.sqrt inbuilt function(--method math)')
 
 
+   
 
 
   
